70b9dc9717aeddfc7316a99c0ddb69bbf13821d1
jeacelo <jeacelo94@gmail.com>
2016-10-20 01:42:11 +0200

    añadido pwm y modificacion de intensidad en topic 1 (no funciona)

--- main.c
+++ main.c
@@ -150,6 +150,25 @@ typedef enum events
     BROKER_DISCONNECTION
 }osi_messages;
 
+// The PWM works based on the following settings:
+//     Timer reload interval -> determines the time period of one cycle
+//     Timer match value -> determines the duty cycle
+//                          range [0, timer reload interval]
+// The computation of the timer reload interval and dutycycle granularity
+// is as described below:
+// Timer tick frequency = 80 Mhz = 80000000 cycles/sec
+// For a time period of 0.5 ms,
+//      Timer reload interval = 80000000/2000 = 40000 cycles
+// To support steps of duty cycle update from [0, 255]
+//      duty cycle granularity = ceil(40000/255) = 157
+// Based on duty cycle granularity,
+//      New Timer reload interval = 255*157 = 40035
+//      New time period = 0.5004375 ms
+//      Timer match value = (update[0, 255] * duty cycle granularity)
+//
+#define TIMER_INTERVAL_RELOAD   40035 /* =(255*157) */
+#define DUTYCYCLE_GRANULARITY   157
+
 //*****************************************************************************
 //                      LOCAL FUNCTION PROTOTYPES
 //*****************************************************************************
@@ -168,6 +187,10 @@ void LedTimerDeinitStop();
 void BoardInit(void);
 static void DisplayBanner(char * AppName);
 void MqttClient(void *pvParameters);
+void UpdateDutyCycle(unsigned long ulBase, unsigned long ulTimer, unsigned char ucLevel);
+void SetupTimerPWMMode(unsigned long ulBase, unsigned long ulTimer, unsigned long ulConfig, unsigned char ucInvert);
+void InitPWMModules();
+void DeInitPWMModules();
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- Start
 //*****************************************************************************
@@ -269,7 +292,11 @@ Mqtt_Recv(void *app_hndl, const char  *topstr, long top_len, const void *payload
 
     if(strncmp(output_str,TOPIC1, top_len) == 0)
     {
-        ToggleLedState(LED1);
+    	//ToggleLedState(LED1);
+    	UpdateDutyCycle(TIMERA2_BASE, TIMER_B, 50);
+    	UpdateDutyCycle(TIMERA3_BASE, TIMER_B, 100);
+    	UpdateDutyCycle(TIMERA3_BASE, TIMER_A, 254);
+    	MAP_UtilsDelay(8000000);
     }
     else if(strncmp(output_str,TOPIC2, top_len) == 0)
     {
@@ -580,10 +607,10 @@ void BoardInit(void)
     // Set vector table base
     //
     #if defined(ccs)
-        IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
+        MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
     #endif
     #if defined(ewarm)
-        IntVTableBaseSet((unsigned long)&__vector_table);
+        MAP_IntVTableBaseSet((unsigned long)&__vector_table);
     #endif
     #endif
     //
@@ -910,6 +937,136 @@ end:
     LOOP_FOREVER();
 }
 
+//
+//! Update the dutycycle of the PWM timer
+//!
+//! \param ulBase is the base address of the timer to be configured
+//! \param ulTimer is the timer to be setup (TIMER_A or  TIMER_B)
+//! \param ucLevel translates to duty cycle settings (0:255)
+//!
+//! This function
+//!    1. The specified timer is setup to operate as PWM
+//!
+//! \return None.
+//
+//****************************************************************************
+void UpdateDutyCycle(unsigned long ulBase, unsigned long ulTimer,
+                     unsigned char ucLevel)
+{
+    //
+    // Match value is updated to reflect the new dutycycle settings
+    //
+    MAP_TimerMatchSet(ulBase,ulTimer,(ucLevel*DUTYCYCLE_GRANULARITY));
+}
+
+//****************************************************************************
+//
+//! Setup the timer in PWM mode
+//!
+//! \param ulBase is the base address of the timer to be configured
+//! \param ulTimer is the timer to be setup (TIMER_A or  TIMER_B)
+//! \param ulConfig is the timer configuration setting
+//! \param ucInvert is to select the inversion of the output
+//!
+//! This function
+//!    1. The specified timer is setup to operate as PWM
+//!
+//! \return None.
+//
+//****************************************************************************
+void SetupTimerPWMMode(unsigned long ulBase, unsigned long ulTimer,
+                       unsigned long ulConfig, unsigned char ucInvert)
+{
+    //
+    // Set GPT - Configured Timer in PWM mode.
+    //
+    MAP_TimerConfigure(ulBase,ulConfig);
+    MAP_TimerPrescaleSet(ulBase,ulTimer,0);
+
+    //
+    // Inverting the timer output if required
+    //
+    MAP_TimerControlLevel(ulBase,ulTimer,ucInvert);
+
+    //
+    // Load value set to ~0.5 ms time period
+    //
+    MAP_TimerLoadSet(ulBase,ulTimer,TIMER_INTERVAL_RELOAD);
+
+    //
+    // Match value set so as to output level 0
+    //
+    MAP_TimerMatchSet(ulBase,ulTimer,TIMER_INTERVAL_RELOAD);
+
+}
+
+//****************************************************************************
+//
+//! Sets up the identified timers as PWM to drive the peripherals
+//!
+//! \param none
+//!
+//! This function sets up the folowing
+//!    1. TIMERA2 (TIMER B) as RED of RGB light
+//!    2. TIMERA3 (TIMER B) as YELLOW of RGB light
+//!    3. TIMERA3 (TIMER A) as GREEN of RGB light
+//!
+//! \return None.
+//
+//****************************************************************************
+void InitPWMModules()
+{
+    //
+    // Initialization of timers to generate PWM output
+    //
+    MAP_PRCMPeripheralClkEnable(PRCM_TIMERA2, PRCM_RUN_MODE_CLK);
+    MAP_PRCMPeripheralClkEnable(PRCM_TIMERA3, PRCM_RUN_MODE_CLK);
+
+    //
+    // TIMERA2 (TIMER B) as RED of RGB light. GPIO 9 --> PWM_5
+    //
+    SetupTimerPWMMode(TIMERA2_BASE, TIMER_B,
+            (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_B_PWM), 1);
+    //
+    // TIMERA3 (TIMER B) as YELLOW of RGB light. GPIO 10 --> PWM_6
+    //
+    SetupTimerPWMMode(TIMERA3_BASE, TIMER_A,
+            (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM), 1);
+    //
+    // TIMERA3 (TIMER A) as GREEN of RGB light. GPIO 11 --> PWM_7
+    //
+    SetupTimerPWMMode(TIMERA3_BASE, TIMER_B,
+            (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM), 1);
+
+    MAP_TimerEnable(TIMERA2_BASE,TIMER_B);
+    MAP_TimerEnable(TIMERA3_BASE,TIMER_A);
+    MAP_TimerEnable(TIMERA3_BASE,TIMER_B);
+}
+
+//****************************************************************************
+//
+//! Disables the timer PWMs
+//!
+//! \param none
+//!
+//! This function disables the timers used
+//!
+//! \return None.
+//
+//****************************************************************************
+void DeInitPWMModules()
+{
+    //
+    // Disable the peripherals
+    //
+    MAP_TimerDisable(TIMERA2_BASE, TIMER_B);
+    MAP_TimerDisable(TIMERA3_BASE, TIMER_A);
+    MAP_TimerDisable(TIMERA3_BASE, TIMER_B);
+    MAP_PRCMPeripheralClkDisable(PRCM_TIMERA2, PRCM_RUN_MODE_CLK);
+    MAP_PRCMPeripheralClkDisable(PRCM_TIMERA3, PRCM_RUN_MODE_CLK);
+}
+
+
 //*****************************************************************************
 //
 //! Main 
@@ -940,6 +1097,11 @@ void main()
     // Configuring UART
     //
     InitTerm();
+
+    //
+    // Initialize the PWMs used for driving the LEDs
+    //
+    InitPWMModules();
 
     //
     // Display Application Banner
